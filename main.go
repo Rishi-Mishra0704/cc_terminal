b7d629716b7c74a7f8d3e8b20c76fb05be44aa41
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-02-13 14:52:02 +0530

    initialize db and add user model

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"cc_terminal/models"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db  *gorm.DB
+	err error
+)
+
+func InitDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=rishi password=1111 dbname=cc_terminal port=5432 sslmode=disable"
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return db, nil
+}
+
+func main() {
+	db, err := InitDB()
+	if err != nil {
+		panic("failed to connect database")
+	}
+	// Migrate the User model
+	db.AutoMigrate(&models.User{})
+}
---
+++ models/user.go
@@ -0,0 +1,9 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Email    string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+}
